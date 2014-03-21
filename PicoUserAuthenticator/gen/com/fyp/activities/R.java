631471a5bd43e7ee45d54f0d947f4829a025b613
Cristian Toader <cristian.matei.toader@gmail.com>
2014-03-21 15:37:44 +0200

    Stopped attempt to use javacv - there was no point as the precompiled .so files were not matching the architecture of the phone I use (even the android ones)

--- PicoUserAuthenticator/gen/com/fyp/activities/R.java
+++ PicoUserAuthenticator/gen/com/fyp/activities/R.java
@@ -19,7 +19,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int buttonBarStyle=0x7f010000;
     }
     public static final class color {
-        public static final int black_overlay=0x7f040000;
+        public static final int black_overlay=0x7f060000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -44,18 +44,38 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int activity_recognito=0x7f030002;
     }
     public static final class string {
-        public static final int Play=0x7f050007;
-        public static final int Recognito=0x7f050005;
-        public static final int Record=0x7f050006;
-        public static final int Test=0x7f050008;
-        public static final int app_name=0x7f050000;
-        public static final int dummy_button=0x7f050001;
-        public static final int pause=0x7f050003;
-        public static final int start=0x7f050002;
-        public static final int stop=0x7f050004;
+        public static final int Play=0x7f040007;
+        public static final int Recognito=0x7f040005;
+        public static final int Record=0x7f040006;
+        public static final int Test=0x7f040008;
+        public static final int app_name=0x7f040000;
+        public static final int dummy_button=0x7f040001;
+        public static final int pause=0x7f040003;
+        public static final int start=0x7f040002;
+        public static final int stop=0x7f040004;
     }
     public static final class style {
         /** 
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
+
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
 
@@ -79,15 +99,17 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int ButtonBar=0x7f060003;
-        public static final int ButtonBarButton=0x7f060004;
-        public static final int FullscreenActionBarStyle=0x7f060005;
-        public static final int FullscreenTheme=0x7f060002;
+        public static final int AppTheme=0x7f050001;
+        public static final int ButtonBar=0x7f050003;
+        public static final int ButtonBarButton=0x7f050004;
+        public static final int FullscreenActionBarStyle=0x7f050005;
+        public static final int FullscreenTheme=0x7f050002;
     }
     public static final class styleable {
         /** 
--- PicoUserAuthenticator/src/com/fyp/authenticator/face/FaceDAO.java
+++ PicoUserAuthenticator/src/com/fyp/authenticator/face/FaceDAO.java
@@ -1,45 +1,26 @@
 package com.fyp.authenticator.face;
 
-import com.googlecode.javacv.cpp.opencv_core;
-
-import static com.googlecode.javacv.cpp.opencv_highgui.*;
-import static com.googlecode.javacv.cpp.opencv_core.*;
-import static com.googlecode.javacv.cpp.opencv_imgproc.*;
-import static com.googlecode.javacv.cpp.opencv_contrib.*;
-
-import android.util.Log;
 
 public class FaceDAO {
 
-	FaceRecognizer recognizer = null;
 
 	public FaceDAO() {
-		this.recognizer = createEigenFaceRecognizer();
 //		this.trainFaceRecognizer();
 	}
 
 	public void trainFaceRecognizer() {
 		int labels[] = new int[1];
-		MatVector images = new MatVector(1);
-		
-		IplImage img;
-    IplImage grayImg;
     
 		{
-			img = cvLoadImage("absolute path");
       
       // Convert image to grayscale:
-      grayImg = IplImage.create(img.width(), img.height(), IPL_DEPTH_8U, 1);
-      cvCvtColor(img, grayImg, CV_BGR2GRAY);
       
       // Append it in the image list:
-      images.put(0, grayImg);
       
       // And in the labels list:
       labels[0] = 1;
 		}
 		
-		this.recognizer.train(images, labels);
 	}
 
 	public double getMatch(Object faceObject) {
