3302149b44fcefb086571fcb7047a40c538c33ef
Cristian Toader <cristian.matei.toader@gmail.com>
2014-03-24 16:00:22 +0200

    Initial commit of face detection service using javafaces - still need to crop faces for library - still need to create owner setter acticity

--- PicoUserAuthenticator/src/com/fyp/authenticator/face/FaceDAO.java
+++ PicoUserAuthenticator/src/com/fyp/authenticator/face/FaceDAO.java
@@ -1,31 +1,46 @@
 package com.fyp.authenticator.face;
 
+import android.graphics.Bitmap;
+import android.util.Log;
+import facerecognition.javafaces.FaceRec;
+import facerecognition.javafaces.MatchResult;
 
 public class FaceDAO {
 
+	private FaceRec faceRec = null;
+
+	private String trainDirectory = null;
+	
+	private static final String OWNER_IMG = "owner.png";
+	private static final String NUM_FACES = "1";
+	private static final String THRESHOLD = "2";
 
 	public FaceDAO() {
-//		this.trainFaceRecognizer();
+		this.faceRec = new FaceRec();
+		// TODO;
+		this.trainDirectory = "TODO";
+
+		this.trainFaceRecognizer();
 	}
 
 	public void trainFaceRecognizer() {
-		int labels[] = new int[1];
-    
-		{
-      
-      // Convert image to grayscale:
-      
-      // Append it in the image list:
-      
-      // And in the labels list:
-      labels[0] = 1;
-		}
-		
+		this.faceRec.processSelections(OWNER_IMG, trainDirectory, NUM_FACES,
+				THRESHOLD);
 	}
 
-	public double getMatch(Object faceObject) {
-		; // TODO:
-		return 0;
+	public double getMatch(Bitmap faceObject) {
+		MatchResult r = this.faceRec.findMatchResult(faceObject,
+				Integer.parseInt(NUM_FACES), Double.parseDouble(THRESHOLD));
+
+		if (r.getMatchSuccess() == false) {
+			Log.d("FaceDAO", "no match");
+		}
+
+		if (r.getMatchMessage() != null) {
+			Log.d("FaceDAO", r.getMatchMessage());
+		}
+		
+		return r.getMatchDistance();
 	}
 
 }
--- PicoUserAuthenticator/src/com/fyp/authenticator/face/FaceService.java
+++ PicoUserAuthenticator/src/com/fyp/authenticator/face/FaceService.java
@@ -1,5 +1,15 @@
 package com.fyp.authenticator.face;
 
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
 import android.os.Message;
 import android.os.RemoteException;
 import android.util.Log;
@@ -12,6 +22,11 @@ public class FaceService extends AuthMechService {
 
 	@Override
 	public void onCreate() {
+		// TODO: figure out how to better support this.
+		if (!isSupported()) {
+			throw new RuntimeException("Camera not supported!");
+		}
+
 		if (this.faceThread == null) {
 			this.faceThread = new AuthenticatorThread();
 			this.faceThread.startThread();
@@ -26,27 +41,52 @@ public class FaceService extends AuthMechService {
 		}
 	}
 
+	private boolean isSupported() {
+		if (getApplicationContext().getPackageManager().hasSystemFeature(
+				PackageManager.FEATURE_CAMERA)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	private class AuthenticatorThread extends Thread {
 		private volatile boolean running = false;
+		private volatile AtomicBoolean faceReady = new AtomicBoolean(false);
+		
 		private FaceDAO dao = new FaceDAO();
 
-		private static final int AUTH_PERIOD = 1 * 1000;
+		private static final int AUTH_PERIOD = 3 * 1000;
 
+		private Camera camera = null;
+		private Bitmap picture = null;
+		
 		public void run() {
 
 			while (this.running) {
 				try {
+					Log.d("FaceService", "starting loop");
 					int score = 0;
-					
-					Thread.sleep(AUTH_PERIOD);
-					// TODO: get image data
 
-					// TODO: get match based on image
-					score = (int) Math.floor(this.dao.getMatch(null));
+					// taking picture
+					Thread.sleep(AUTH_PERIOD);
+					Log.d("FaceService", "taking picture");
+					this.initialiseCamera();
+					this.camera.takePicture(shutterCallback, null, rawCallback);
+
+					// get match based on image
+					while(this.faceReady.compareAndSet(true, false))
+						Log.d("FaceService", "Not ready!");
+					
+					double dscore = this.dao.getMatch(this.picture);
+					Log.d("FaceService", "dscore: " + dscore);
+					
+					score = (int) Math.floor(dscore);
 
 					// sending the score
 					Log.d(this.getClass().toString(), "Sending score " + score + "...");
-					clientWriter.send(Message.obtain(null, AUTH_MECH_GET_STATUS, score, 0));
+					clientWriter.send(Message
+							.obtain(null, AUTH_MECH_GET_STATUS, score, 0));
 
 				} catch (InterruptedException e) {
 					e.printStackTrace();
@@ -70,6 +110,55 @@ public class FaceService extends AuthMechService {
 				e.printStackTrace();
 			}
 		}
+		
+		private void initialiseCamera() {
+			try {
+				this.camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT);
+				this.camera.setPreviewTexture(new SurfaceTexture(1));
+				this.camera.startPreview();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		PictureCallback rawCallback = new PictureCallback() {
+			public void onPictureTaken(byte[] data, Camera camera) {
+				Log.d("CAMERA", "onPictureTaken - raw");
+				
+				camera.stopPreview();
+				camera.release();
+				
+				Bitmap bmpRes = null;
+				
+				BitmapFactory.Options options = new BitmapFactory.Options();
+				options.inSampleSize = 8;
+				
+				if (data == null) {
+					Log.e("FaceService", "null data");
+					return;
+				}
+				
+				bmpRes = BitmapFactory.decodeByteArray(data, 0, data.length, options);
+				if (bmpRes == null) {
+					Log.e("FaceService", "Decoded bmp is null!");
+					return;
+				} else {
+					Log.d("FaceService", "Decoded bmp is ok!");
+				}
+				
+				AuthenticatorThread.this.picture = bmpRes;
+				AuthenticatorThread.this.faceReady.set(true);
+
+				Log.d("FaceService", "normal exit-");
+			}
+			
+		};
+		
+		ShutterCallback shutterCallback = new ShutterCallback() {
+			public void onShutter() {
+				Log.i("CAMERA", "onShutter'd");
+			}
+		};
+		
 	}
 
 }
